35210676910a584b57ab6df592be918b161a1f70
Farhod Gofurov <fg325609@gmail.com>
2026-05-07 10:07:43 +0500

    jjjjj

--- src/companents/Card/Card.jsx
+++ src/companents/Card/Card.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react'
-import "../Card.scss";
 import { useState } from 'react';
 
 function Card  (){
     const [data, setData] = useState([]);
     useEffect(()=>{
         fetch("https://69e05e1129c070e6597b67e3.mockapi.io/students")
-    })
+        .then(data => data.json()
+        .then(data =>{
+          setData(data)
+          console.log(data);  
+        })
+      )
+    }, )
+    
   return (
     <div className='one'>
 
